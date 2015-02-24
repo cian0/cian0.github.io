e4f29113e6a58699faa3adba77c3c778b2e4ab26
Ian Icasiano <icasiano_ian@yahoo.com>
2015-02-24 09:57:45 +0800

    added elearning mockups

---
+++ elearning/assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+		loop: false,
+		paginationSpeed: 400,
+		singleItem: true,
+		items: 1,
+	    baseClass : "owl-carousel",
+	    theme : "owl-theme",
+		autoHeight: false
+	});
+});
---
+++ elearning/assets/js/app/login.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$('.sign-in').click(function () {
+        window.location.href = 'trainee.html';
+    });
+});
