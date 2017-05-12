b57a52d0427ee118f79671cdf75d04cb7af4d60f
Ian Icasiano <cyril.icasiano@gmail.com>
2017-05-12 15:03:44 +0800

    added games

---
+++ game_2/config/script.js
@@ -0,0 +1,13 @@
+/*ga*/
+var brandName ="";
+var folderName ="";
+var metric = false;
+var is_fragment_flag = true; // set this for analytics convo type if fragment ( multiple question submit ) or not( one question submit ).
+/*configurables*/
+var isTutorial = false; // set to true/false toggle to load instruction on page load 
+var isThankYou = false; // set to true/false toggle to load thank you message in the end 
+var delayResponse = 1000; // 1000 millisecond = 1 second - Set timer delay for responses
+//var BotNextMsgTimer = 1500; // 1000 millisecond = 1 second - Set timer delay for bot next message / question on the page
+//var myResponseTimer = 2000; // 1000 millisecond = 1 second - Set timer delay for user response on the page
+
+
