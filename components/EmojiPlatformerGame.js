1c75d09486962a65021a4ef06b9e7338f5e11b67
Your Name <you@example.com>
2024-08-20 17:22:03 +0000

    feat: Add dynamic import for EmojiPlatformerGame component

--- components/EmojiPlatformerGame.js
+++ components/EmojiPlatformerGame.js
@@ -1,4 +1,4 @@
-/import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import dynamic from 'next/dynamic';
 import DPadController from './DPadController';
 
