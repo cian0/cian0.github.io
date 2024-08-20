fa3396c5bfdcccdbe1a118db424962d88a7c955b
Your Name <you@example.com>
2024-08-20 17:16:24 +0000

    feat: Add state management to EmojiPlatformerGame component

--- components/EmojiPlatformerGame.js
+++ components/EmojiPlatformerGame.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import dynamic from 'next/dynamic';
 
 const EmojiPlatformerGame = () => {
